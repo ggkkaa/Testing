91dfc13f1db218b960af34a7749350264f371088
ggkkaa <33029337+ggkkaa@users.noreply.github.com>
2023-07-29 14:00:18 +0200

    Add files via upload

--- player.js
+++ player.js
@@ -5,10 +5,21 @@ export class Player {
     this.height = 91.3;
     this.x = 0;
     this.y = this.game.height - this.height;
+    this.vy = 0
     this.image = document.getElementById('player');
+    this.speed = 0;
+    this.maxSpeed = 10;
     }
-    update(){
-        this.x++
+    update(input){
+        //horizontal movement
+        this.x += this.speed;
+        if (input.includes('d')) this.speed = this.maxSpeed
+        else if (input.includes('a')) this.speed = -this.maxSpeed
+        else this.speed = 0;
+        if (this.x < 0) this.x = 0
+        if (this.x > this.game.width - this.width) this.x = this.game.width - this.width
+        //vertical movement
+
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
